d27a3759cc6b7ffd95de69c01b838f0e765a6363
Daniel <117210989+danielTongu@users.noreply.github.com>
2025-04-28 11:50:24 -0700

    deleted files

--- vmware_dashboard/utils/config.py
+++
@@ -1,9 +0,0 @@
-"""
-config.py
----------
-Global feature flags.
-Toggle USE_MOCK to switch between mock data and production mode.
-"""
-
-# Set to True for mock/testing mode; False for real scripts & storage
-USE_MOCK = True
