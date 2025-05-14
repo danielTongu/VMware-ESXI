91c0027c18d109cbbd0b741a0ae28a6685b2c3c5
Daniel Tongu <tongud@cwu.edu>
2025-05-14 08:51:13 -0700

    Fixed 'ForeColor' bug

--- Views/LoginView.ps1
+++ Views/LoginView.ps1
@@ -277,7 +277,7 @@ function Handle-Login {
             }
             $securePwd | Export-Clixml -Path $global:Paths.Credentials -Force
         } elseif (Test-Path $global:Paths.Credentials) {
-            Remove-Item $global:AppConfig.Paths.Credentials -ErrorAction SilentlyContinue
+            Remove-Item $global:Paths.Credentials -ErrorAction SilentlyContinue
         }
 
         $script:LoginResult = $true
