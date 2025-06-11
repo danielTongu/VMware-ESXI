63b5ba3c99f3999e0d92e5ea544fbf4f65f610cc
Daniel Tongu <tongud@cwu.edu>
2025-06-11 15:02:12 -0700

    Adjust path to your views directory

--- Controllers/MainView.ps1
+++ Controllers/MainView.ps1
@@ -142,7 +142,7 @@ function New-AppWindow {
     $script:ContentPanel.Padding = [System.Windows.Forms.Padding]::Empty
     $splitContainer.Panel2.Controls.Add($script:ContentPanel)
 
-    $scriptDir = "$PSScriptRoot\..\Views"
+    $scriptDir = "$PSScriptRoot\..\Views" # Adjust path to your views directory
     $script:DashboardButton = New-NavButton -Text 'Dashboard' -ScriptPath "$scriptDir\DashboardView.ps1" -TargetPanel $script:ContentPanel
     $script:ClassesButton = New-NavButton -Text 'Class Manager' -ScriptPath "$scriptDir\ClassesView.ps1" -TargetPanel $script:ContentPanel
     $script:VMsButton = New-NavButton -Text 'Virtual Machines' -ScriptPath "$scriptDir\VMsView.ps1" -TargetPanel $script:ContentPanel
